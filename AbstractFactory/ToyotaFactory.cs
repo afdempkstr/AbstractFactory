0246617fa0311e2bc4dcf826e3e411c72ed9e255
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-11 21:33:43 +0200

    Added car

--- AbstractFactory/ToyotaFactory.cs
+++ AbstractFactory/ToyotaFactory.cs
@@ -82,6 +82,22 @@ namespace AbstractFactory
         }
     }
 
+    class Car
+    {
+        public Body Body { get; }
+        public Engine Engine { get; }
 
+        public Car(Body body, Engine engine)
+        {
+            Body = body;
+            Engine = engine;
+        }
 
+        public override string ToString()
+        {
+            return $"{Body} with a weight of {Body.Weight}kg and a {Engine}";
+        }
+    }
+
+    
 }
